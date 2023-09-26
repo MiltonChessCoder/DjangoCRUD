09ec054d5919654ebaa142906412b2610de0664e
Milton <miltonsoftwareengineer@gmail.com>
2023-09-26 15:27:03 +0300

    completed app

--- myToDo/urls.py
+++ myToDo/urls.py
@@ -6,7 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name="list"),
     path('update_task/<str:pk>/', views.updateTask, name="update_task"),
-    path('delete/<str:pk>/', views.deleteTask, name="delete_task")
+    path('delete/<str:pk>/', views.deleteTask, name="delete_task"),
+
 ]
 
 
